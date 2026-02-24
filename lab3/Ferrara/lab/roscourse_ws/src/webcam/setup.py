c4c975bba82c1054ce43e5470d0c2c2764864088
Sal Ferrara <salferrara55555@gmail.com>
2026-02-25 06:44:30 +0800

    updating lab 3

--- lab3/Ferrara/lab/roscourse_ws/src/webcam/setup.py
+++ lab3/Ferrara/lab/roscourse_ws/src/webcam/setup.py
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'webcam_pub = webcam.cam_pub:main',
-        'webcam_sub = webcam.cam_sub:main',
+        	'webcam_pub = webcam.cam_pub:main',
+        	'webcam_sub = webcam.cam_sub:main',
         ],
     },
 )
