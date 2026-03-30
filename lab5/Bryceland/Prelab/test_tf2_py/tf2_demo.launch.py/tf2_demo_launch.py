66d62d3eb128a1635cfc006a1d6ef3cadb7c8bc2
James Bryceland <brycej.rpi@gmail.com>
2026-03-31 03:40:09 +0800

    Added Lab 5

---
+++ lab5/Bryceland/Prelab/test_tf2_py/tf2_demo.launch.py/tf2_demo_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    target_frame = DeclareLaunchArgument('target_frame', default_value='turtle1', description='Target frame name')
+    
+    broadcaster2 = Node(package='test_tf2_py', executable='turtle_tf2_broadcaster', name='broadcaster2', parameters=[{'turtlename': 'turtle2'}])
+    
+    listener = Node(package='test_tf2_py', executable='turtle_tf2_listener', name='listener', parameters=[{'target_frame': LaunchConfiguration('target_frame')}])
+
+ 
+    return LaunchDescription([
+        Node(package='turtlesim', 
+            executable='turtlesim_node',
+            name = 'sim'),
+        Node(package='test_tf2_py',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster1',
+            parameters=[{'turtlename': 'turtle1'}])
+        ])
