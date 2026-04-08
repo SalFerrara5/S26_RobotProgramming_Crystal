9b1ce762b462361521af4f8a179053fe14ff4045
Sal Ferrara <salferrara55555@gmail.com>
2026-04-08 23:31:58 +0800

    lab 6 some more

--- lab6/Ferrara/lab/roscourse_sim_ws/src/my_robot/setup.py
+++ lab6/Ferrara/lab/roscourse_sim_ws/src/my_robot/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'my_robot'
 
@@ -7,9 +9,18 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        # URDF files
+        (os.path.join('share', package_name), glob('urdf/*')),
+
+        # Launch files
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+
+        # RViz configs
+        (os.path.join('share', package_name), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
